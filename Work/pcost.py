2f7127b1a5704a2e5699577d1222e36fbab5d55d
Khels <denismahnuk@gmail.com>
2020-09-30 22:02:49 +0300

    First changes.

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,30 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
+import sys
+
+def portfolio_cost(filename):
+    total_cost = 0.0
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            try:
+                nshares = int(row[1])
+                price = float(row[2])
+                total_cost += nshares * price
+            # This catches errors in int() and float() conversions above
+            except ValueError:
+                print('Bad row:', row)
+
+    return total_cost
+    
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,51 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+
+def read_portfolio(filename):
+    '''Computes the total cost (shares*price) of a portfolio file'''
+    portfolio = []
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            holding = {
+                'name' : row[0],
+                'shares' : int(row[1]),
+                'price' : float(row[2])
+            }
+            portfolio.append(holding)
+    return portfolio
+    
+def read_prices(filename):
+    holding = {}
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                holding[row[0]] = float(row[1])
+            except IndexError:
+                pass
+            
+    return holding
+    
+def make_report(portfolio, prices):
+    report = []
+    for p in portfolio:
+        tupl = (p['name'], p['shares'], prices[p['name']], prices[p['name']] - p['price'])
+        report.append(tupl)
+    return report
+    
+portfolio = read_portfolio('Data/portfolio.csv')
+prices = read_prices('Data/prices.csv')
+report = make_report(portfolio, prices)
+    
+headers = ('Name', 'Shares', 'Price', 'Change')
+print(f'%10s %10s %10s %10s' %headers)
+#print(f'{"":-<10s} {"":-<10s} {"":-<10s} {"":-<10s}')
+print(('-' * 10 + ' ') * len(headers))
+
+for name, shares, price, change in report:
+    line = '$' + '%0.2f' % price
+    print(f'{name:>10s} {shares:>10d} {line:>10s} {change:>10.2f}')
